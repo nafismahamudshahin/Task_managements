2a883288e7ebd0ad81061d45698f735d4ec0ddee
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-11-02 00:47:35 +0600

    update all

--- task_management/settings.py
+++ task_management/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-5p@1ke2%vd%wuc^4xa+dfftjckbh+swywoa@*u0#hwtm@=n01('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -66,18 +66,18 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': config("ENGINE"),
-        'NAME': BASE_DIR / config("NAME"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': config("ENGINE"),
+#         'NAME': BASE_DIR / config("NAME"),
+#     }
+# }
 
 
 # Password validation
